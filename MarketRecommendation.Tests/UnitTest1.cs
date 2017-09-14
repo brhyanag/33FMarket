7c445a45bc71db613d2614251916e89d14269da2
brhyana <brhyana_01_gonzales@hotmail.com>
2017-09-14 21:52:49 +0200

    Creacion Proyecto

---
+++ MarketRecommendation.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarketRecommendation.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MarketRecommendation/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarketRecommendation.Startup))]
+namespace MarketRecommendation
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
